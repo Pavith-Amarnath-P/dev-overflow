ae11ecc8280cceb7c3ff41db91008a2d2be8c845
Pavith Amarnath P <greatfulpavith@gmail.com>
2024-10-30 11:37:37 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="text-5xl text-violet-700 font-black">
+      Welcome to the World Of Next Js.
+    </h1>
+  );
+}
