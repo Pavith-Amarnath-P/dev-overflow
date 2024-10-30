676048bae1e43d8e968d9686af745c999e7272e5
Pavith Amarnath P <greatfulpavith@gmail.com>
2024-10-30 12:40:32 +0530

    all setup complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-5xl font-black text-violet-700">
+    <h1 className="h1-bold text-5xl font-black text-violet-700">
       Welcome to the World Of Next Js.
     </h1>
   );
